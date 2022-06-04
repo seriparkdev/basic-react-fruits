e8d6ba8fe5ee6b8c4be0bab2496cae2f196302ab
seripark13 <qkrtpfl9228@gmail.com>
2022-06-04 15:11:03 +0900

    완성
    최종

--- src/App.js
+++ src/App.js
@@ -2,60 +2,91 @@ import Modal from "./Modal";
 import { useState } from "react";
 
 function App() {
+  const data = [
+    {
+      name: "복숭아",
+      image: "./img/복숭아.jpg",
+      content: "달달한 복숭아",
+    },
+
+    {
+      name: "포도",
+      image: "./img/포도.jpg",
+      content: "맛있는 포도",
+    },
+
+    {
+      name: "청사과",
+      image: "./img/청사과.png",
+      content: "시원한 청사과",
+    },
+
+    {
+      name: "파인애플",
+      image: "img/파인애플.jpg",
+      content: "신 파인애플",
+    },
+
+    {
+      name: "딸기",
+      image: "img/딸기.jpg",
+      content: "달콤한 딸기",
+    },
+  ];
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
-    
+  const [items, setItems] = useState(data);
+
   function openModalHandler() {
-      setModalIsOpen(true);
+    setModalIsOpen(true);
   }
 
   function closeModalHandler() {
-      setModalIsOpen(false);
+    setModalIsOpen(false);
   }
 
+  function addItemHandler(item) {
+    setItems([...items, item]);
+  }
 
   return (
-    <div>
-    <header>
+    <>
+      <header>
         <div id="title">🧺FRUIT</div>
-    </header>
-    <main>
-    <aside id="sidebar">
-        <button type="button" className="addButton" id="addButton" onClick={openModalHandler}>추가</button>
-        <span>복숭아</span>
-        <span>포도</span>
-        <span>청사과</span>
-        <span>파인애플</span>
-        <span>딸기</span>
-    </aside>
-    <section id="container">
-        <div className="item">
-            <img src="./img/복숭아.jpg" alt="복숭아" />
-            <span>달달한 복숭아</span>
-        </div>
-        <div className="item">
-            <img src="./img/포도.jpg" alt="포도" />
-            <span>맛있는 포도</span>
-        </div>
-        <div className="item">
-            <img src="img/청사과.png" alt="청사과" />
-            <span>시원한 청사과</span>
-        </div>
-        <div className="item">
-            <img src="img/파인애플.jpg" alt="파인애플" />
-            <span>신 파인애플</span>
-        </div>
-        <div className="item">
-            <img src="img/딸기.jpg" alt="딸기" />
-            <span>달콤한 딸기</span>
-        </div>
-    </section>
-    
-    { modalIsOpen && <Modal modalIsOpen={modalIsOpen} openModalHandler={openModalHandler} closeModalHandler={closeModalHandler}/> }
+      </header>
+      <main>
+        <aside id="sidebar">
+          <button
+            type="button"
+            className="addButton"
+            id="addButton"
+            onClick={openModalHandler}
+          >
+            추가
+          </button>
+          {items.map((item) => (
+            <span key={item.name}>{item.name}</span>
+          ))}
+        </aside>
 
-        
-    </main>
-    </div>
+        <section id="container">
+          {items.map((item) => (
+            <div className="item" key={item.name}>
+              <img src={item.image} alt={item.name} />
+              <span>{item.content}</span>
+            </div>
+          ))}
+        </section>
+
+        {modalIsOpen && 
+          <Modal
+            addItemHandler={addItemHandler}
+            openModalHandler={openModalHandler}
+            closeModalHandler={closeModalHandler}
+          />
+        }
+      </main>
+    </>
   );
 }
 
--- src/Modal.js
+++ src/Modal.js
@@ -1,22 +1,74 @@
-function Modal(props) {
- 
+import { useRef } from "react";
 
-    
-    return(
-        <>
-        <div className="modalOverlay" id="modal" onClick={props.closeModalHandler}></div>
-        <form className="modal" id="addForm">
-            <span className="xButton" id="xButton" onClick={props.closeModalHandler}>X</span>
-            <label htmlFor="name" className="label">이름</label>
-            <input type="text" id="name" placeholder="바나나" className="input" required/>
-            <label htmlFor="image" className="label">이미지</label>
-            <input type="text" id="image" placeholder="URL" className="input" required />
-            <label htmlFor="content" className="label">내용</label>
-            <input type="text" id="content" placeholder="얼려먹으면 맛있는 바나나" className="input" required/>
-            <button type="submit" className="submitbtn">저장</button>
-        </form>
-        </>
-    );
+function Modal(props) {
+  const enteredName = useRef();
+  const enteredImg = useRef();
+  const enteredContent = useRef();
+
+  function onSubmit(e) {
+    e.preventDefault();
+    const item = {
+      name: enteredName.current.value,
+      image: enteredImg.current.value,
+      content: enteredContent.current.value,
+    };
+    props.addItemHandler(item);
+  }
+
+  return (
+    <>
+      <div
+        className="modalOverlay"
+        id="modal"
+        onClick={props.closeModalHandler}
+      ></div>
+      <form className="modal" id="addForm" onSubmit={onSubmit}>
+        <span
+          className="xButton"
+          id="xButton"
+          onClick={props.closeModalHandler}
+        >
+          X
+        </span>
+        <label htmlFor="name" className="label">
+          이름
+        </label>
+        <input
+          type="text"
+          id="name"
+          placeholder="바나나"
+          className="input"
+          required
+          ref={enteredName}
+        />
+        <label htmlFor="image" className="label">
+          이미지
+        </label>
+        <input
+          type="text"
+          id="image"
+          placeholder="URL"
+          className="input"
+          required
+          ref={enteredImg}
+        />
+        <label htmlFor="content" className="label">
+          내용
+        </label>
+        <input
+          type="text"
+          id="content"
+          placeholder="얼려먹으면 맛있는 바나나"
+          className="input"
+          required
+          ref={enteredContent}
+        />
+        <button type="submit" className="submitbtn">
+          저장
+        </button>
+      </form>
+    </>
+  );
 }
 
 export default Modal;
